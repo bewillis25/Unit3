42ce169c627fefccaa57df57d409b421c2438882
bewillis25 <bewillis25@students.bmchs.org>
2022-02-15 09:48:00 -0600

    Lab 3.02

--- Lab 3.02.py
+++ Lab 3.02.py
@@ -10,8 +10,11 @@ Create a function, birthday_song, that prints out the happy birthday song to wha
   # Purpose: birthday_song prints out a personalized birthday song
   # Arguments: name, string
   # Returns: none
+  name = input("What is your name? ")
   def birthday_song(name):
-     #your code goes here
+    print(f"Happy Birthday to you, Happy Birthday to you, Happy Birthday dear {name}, Happy Birthday to you.")
+  birthday_song(name)
+     
 
 Lab Exercise 2
 ---------------
@@ -30,6 +33,32 @@ What is different about passing in a list as an argument?
 Read about list aliasing in section 3.4 of the associated reading, and write down what is happening in this case.
 Remember, the associated reading is in the "SWBAT" section on moodle!
 '''
-def my_function():
-  print("This is my function")
-my_function()
+
+'''
+# Birthday Song
+name = input("What is your name? ")
+def birthday_song(name):
+  print(f"Happy Birthday to you, Happy Birthday to you, Happy Birthday dear {name}, Happy Birthday to you.")
+birthday_song(name)
+'''
+
+import random
+def random_card():
+  number = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+  suit = ['hearts','diamonds','spades','clubs']
+  random_number = random.choice(number)
+  random_suit = random.choice(suit)
+  card = []
+  card.append(f"{random_number} of {random_suit}")
+  print(card)
+def hand():
+  [random_card(),random_card(),random_card(),random_card(),random_card()]
+hand()
+
+
+
+
+
+
+
+
