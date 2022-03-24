81468393dd2c650b9e990bbfaf9d72f14d0ab522
bewillis25 <bewillis25@students.bmchs.org>
2022-03-24 09:23:02 -0500

    Oregon Trail Rough Draft

--- OregonTrail.py
+++ OregonTrail.py
@@ -1,3 +1,4 @@
+# Initial global variables
 import random
 name = input("What is your name? ")
 month_num = 3
@@ -8,8 +9,9 @@ food_num = 500
 distance_traveled = 0
 miles_to_go = 2000 - distance_traveled
 months_with_31_days = [1, 3, 5, 7, 8, 10, 12]
-first_health_loss = random.randint(1,31)
-second_health_loss = random.randint(1,31)
+first_health_loss = random.randint(1,30)
+second_health_loss = random.randint(1,30)
+# Whether to change month on 30 or 31
 def change_month():
     global day_num
     global month_num
@@ -22,6 +24,7 @@ def change_month():
         if day_num > 30:
             month_num += 1
             day_num -= 30
+# Turns month_num into a name for status function
 def month_name_function():
     global month_num
     global month_name
@@ -45,6 +48,7 @@ def month_name_function():
         month_name = 'November'
     elif month_name == 12:
         month_name = 'December'
+# Tracks how many days pass for a random action
 def add_day(min,max):
     global day_num
     global month_num
@@ -53,15 +57,20 @@ def add_day(min,max):
     global health_num
     global first_health_loss
     global second_health_loss
+    global food_num
     random_days = random.randint(min,max)
-    day_num += random_days
     print(f"{random_days} days have passed.")
-    if first_health_loss >= min or first_health_loss <= max:
+    # Health decreases randomly
+    upper_bound = day_num + random_days
+    if first_health_loss >= day_num and first_health_loss <= upper_bound:
         health_num -= 1
         print("You lost 1 health due to time")
-    if second_health_loss >= min or second_health_loss <= max:
+    if second_health_loss >= day_num and second_health_loss <= upper_bound:
         health_num -= 1
         print("You lost 1 health due to time")
+    day_num += random_days
+    food_num -= random_days*5
+# moves you randomly between 30-60 miles and takes 3-7 days (random)
 def travel():
     global distance_traveled
     global day_num
@@ -69,6 +78,7 @@ def travel():
     distance_traveled += random_distance
     add_day(3,7)
     print(f"You traveled {random_distance} miles.")
+# increases health 1 level (up to 5 maximum) and takes 2-5 days (random)
 def rest():
     global health_num
     global day_num
@@ -79,6 +89,7 @@ def rest():
         health_num += 1
         add_day(2,5)
         print(f"Your health is now {health_num}.")
+# adds 100lbs of food and takes 2-5 days (random)
 def hunt():
     global food_num
     global day_num
@@ -88,6 +99,7 @@ def hunt():
         food_num = 500
     else:
         food_num += 100
+# lists food, health, distance traveled, and date
 def status():
     global distance_traveled
     global food_num
@@ -98,12 +110,14 @@ def status():
     month_name_function()
     print(f"You have {food_num} lbs of food.")
     print(f"You have traveled {distance_traveled} miles.")
+    print(f"You have {2000-distance_traveled} miles to go.")
     print(f"Your health is {health_num}.")
     print(f"The date is {month_name}, {day_num}.")
+# Main game loop that determines the player's action every turn
 def select_action():
     action = ''
-    while health_num > 0 and action != 'quit':
-        action = input('Enter a command. Type "help" for a list of commands: ')
+    while health_num > 0 and action != 'quit' and food_num > 0 and month_num < 13 and distance_traveled < 2000:
+        action = input(f"{name}, enter a command. Type 'help' for a list of commands: ")
         if action == 'help':
             print("travel: moves you randomly between 30-60 miles and takes 3-7 days (random) \nrest: increases health 1 level (up to 5 maximum) and takes 2-5 days (random) \nhunt: adds 100lbs of food and takes 2-5 days (random) \nstatus: lists food, health, distance traveled, and day \nhelp: lists all the commands \nquit: will end the game.")
         elif action == 'travel':
@@ -114,11 +128,22 @@ def select_action():
             hunt()
         elif action == 'status':
             status()
+        elif action == 'quit':
+            print("Bye!")
         else:
             print("Not a valid command. Please try again: ")
 select_action()
-
-
+if month_num > 13:
+    print("Sorry, you didn't get there in time")
+    print("GAME OVER")
+if distance_traveled > 2000:
+    print("You made it to Oregon!")
+if health_num == 0:
+    print("Sorry, you have no health left and are dead")
+    print("GAME OVER")
+if food_num == 0:
+    print("You ran out of food and starved to death")
+    print("GAME OVER")
 
 
 
