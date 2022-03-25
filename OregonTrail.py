3bd098b230bfcaaef4cb47499a917a36c25497c4
bewillis25 <bewillis25@students.bmchs.org>
2022-03-25 09:51:05 -0500

    Final Oregon Trail Project with Bonuses

--- OregonTrail.py
+++ OregonTrail.py
@@ -1,3 +1,5 @@
+# Oregon Trail Project. User enters actions simulating a traveler going to Oregon on the Oregon Trail
+
 # Initial global variables
 import random
 name = input("What is your name? ")
@@ -11,6 +13,7 @@ miles_to_go = 2000 - distance_traveled
 months_with_31_days = [1, 3, 5, 7, 8, 10, 12]
 first_health_loss = random.randint(1,30)
 second_health_loss = random.randint(1,30)
+accident = random.randint(1,30)
 # Whether to change month on 30 or 31
 def change_month():
     global day_num
@@ -49,7 +52,7 @@ def month_name_function():
     elif month_name == 12:
         month_name = 'December'
 # Tracks how many days pass for a random action
-def add_day(min,max):
+def add_day(min,max,activity):
     global day_num
     global month_num
     global month_name
@@ -62,21 +65,42 @@ def add_day(min,max):
     print(f"{random_days} days have passed.")
     # Health decreases randomly
     upper_bound = day_num + random_days
-    if first_health_loss >= day_num and first_health_loss <= upper_bound:
+    if first_health_loss >= day_num and first_health_loss < upper_bound:
         health_num -= 1
         print("You lost 1 health due to time")
-    if second_health_loss >= day_num and second_health_loss <= upper_bound:
+    if second_health_loss >= day_num and second_health_loss < upper_bound:
         health_num -= 1
         print("You lost 1 health due to time")
+    # If an accident occurs
+    if accident >= day_num and accident < upper_bound:
+        health_change = random.randint(0,1)
+        which_accident = random.randint(1,2)
+        food_change = random.randint(1,10)
+        random_accident_days = random.randint(1,10)
+        if which_accident == 1:
+            print(f"You had to cross a river and lost {health_change} health and {food_change} lbs of food.")
+        elif accident == 2:
+            print(f"You had dysentary and lost {health_change} health and {food_change} lbs of food.")
+        health_num -= health_change
+        food_num -= food_change
+        day_num += random_accident_days
+    # Update day_num and food eaten
     day_num += random_days
-    food_num -= random_days*5
+    if activity == "hunt":
+        food_num -= random_days*7
+    elif activity == "rest":
+        food_num -= random_days*3
+    elif activity == "normal":
+        food_num -= random_days*5
+    else:
+        food_num -= 0
 # moves you randomly between 30-60 miles and takes 3-7 days (random)
 def travel():
     global distance_traveled
     global day_num
     random_distance = random.randint(30,60)
     distance_traveled += random_distance
-    add_day(3,7)
+    add_day(3,7,"normal")
     print(f"You traveled {random_distance} miles.")
 # increases health 1 level (up to 5 maximum) and takes 2-5 days (random)
 def rest():
@@ -84,24 +108,19 @@ def rest():
     global day_num
     if health_num == 5:
         print("You are already at maximum health.")
-        add_day(0,0)
+        add_day(0,0,"zero")
     elif health_num < 5:
         health_num += 1
-        add_day(2,5)
+        add_day(2,5,"rest")
         print("You gained 1 health during your rest")
         print(f"Your health is now {health_num}.")
 # adds 100lbs of food and takes 2-5 days (random)
 def hunt():
     global food_num
     global day_num
+    add_day(2,5,"hunt")
     food_num += 100
-    add_day(2,5)
-    if food_num > 500:
-        food_num = 500
-        print("You already have 500 lbs of food")
-    else:
-        food_num += 100
-        print("You gained 100 lbs of food")
+    print("You gained 100 lbs of food")
 # lists food, health, distance traveled, and date
 def status():
     global distance_traveled
